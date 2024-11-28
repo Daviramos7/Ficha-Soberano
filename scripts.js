ba7c75d43d660c4bcf353310c91a1d88430c33e6
Daviramos7 <daviramosferr2015@gmail.com>
2024-11-27 23:37:45 -0300

    Código completo

---
+++ scripts.js
@@ -0,0 +1,9 @@
+document.getElementById('show-terms').addEventListener('click', function () {
+    alert(
+      'Termos e Condições:\n\n1. Ao preencher este formulário, você concorda em fornecer informações reais e válidas.\n' +
+      '2. Seus dados serão armazenados e processados de forma segura.\n' +
+      '3. Leia atentamente as regras antes de prosseguir.\n' +
+      '4. Para dúvidas, entre em contato com o suporte.'
+    );
+  });
+  
